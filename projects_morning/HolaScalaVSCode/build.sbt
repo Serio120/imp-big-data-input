fc6a1f6a75528c94b12d9c7e9fbf1740786d0550
Serio120 <113494029+Serio120@users.noreply.github.com>
2026-04-14 12:07:29 +0200

    add folder projets_morning & assets

---
+++ projects_morning/HolaScalaVSCode/build.sbt
@@ -0,0 +1,7 @@
+
+name := "HolaScalaVSCode"
+
+version := "0.1"
+
+// USA ESTA VERSIÓN (es la versión estable de Scala 3)
+scalaVersion := "3.3.1"
---
+++ projects_morning/HolaScalaVSCode/src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hola Scala desde VS Code")
+    main2()
+  }
+
+  def main2(): Unit = {
+    val nombre = "Pedro"
+    println(s"Bienvenido a Scala,$nombre")
+  }
+}
+
+
+ 
